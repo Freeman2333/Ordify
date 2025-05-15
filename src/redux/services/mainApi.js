37fb4accadb4b6d177e57cd96e8e6809b5743b1f
Serhii Popov <serhii.popov@codeit.pro>
2025-05-15 15:15:47 +0300

    env file added

--- src/redux/services/mainApi.js
+++ src/redux/services/mainApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const mainApi = createApi({
   reducerPath: "mainApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3010/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_URL }),
   endpoints: (builder) => ({
     getOrders: builder.query({
       query: () => ({ url: `/orders` }),
