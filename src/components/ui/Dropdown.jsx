a6bbaa35faa7d13eec141ec7d31fb78dd77d2464
Serhii Popov <serhii.popov@codeit.pro>
2025-05-16 22:56:46 +0300

    img alt fix

--- src/components/ui/Dropdown.jsx
+++ src/components/ui/Dropdown.jsx
@@ -37,7 +37,7 @@ const Dropdown = ({
           className={`ml-3 transition-transform duration-300 ${
             isOpen ? "rotate-180" : ""
           }`}
-          alt=""
+          alt="arrow down"
           aria-hidden="true"
         />
       </button>
