60858691a57ebd66f3f7104a8bbf271506e26c21
Serhii Popov <serhii.popov@codeit.pro>
2025-05-19 13:38:18 +0300

    query invalidation added

--- src/redux/services/mainApi.js
+++ src/redux/services/mainApi.js
@@ -3,20 +3,25 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const mainApi = createApi({
   reducerPath: "mainApi",
   baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_URL }),
+  tagTypes: ["Orders"],
   endpoints: (builder) => ({
     getOrders: builder.query({
       query: ({ status }) => ({
         url: `/orders`,
-        params: {
-          status,
-        },
+        params: { status },
       }),
+      providesTags: ["Orders"],
     }),
     getOrder: builder.query({
       query: (id) => ({ url: `/orders/${id}` }),
+      providesTags: (_result, _error, id) => [{ type: "Orders", id }],
     }),
     deleteOrder: builder.mutation({
-      query: (id) => ({ url: `/orders/${id}`, method: "DELETE" }),
+      query: (id) => ({
+        url: `/orders/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Orders"],
     }),
   }),
 });
