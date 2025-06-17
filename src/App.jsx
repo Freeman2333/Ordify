a4092e9b9874c4cd5d28e53d0091127ed2d0be33
Serhii Popov <serhii.popov@codeit.pro>
2025-06-17 16:54:22 +0300

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
+import { Suspense, lazy } from "react";
 import { Navigate, Route, Routes } from "react-router";
 
-import OrdersPage from "./pages/OrdersPage";
-import OrderPage from "./pages/OrderPage";
-import MainLayout from "./layouts/MainLayout";
+const OrdersPage = lazy(() => import("./pages/OrdersPage"));
+const OrderPage = lazy(() => import("./pages/OrderPage"));
+const MainLayout = lazy(() => import("./layouts/MainLayout"));
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/orders" replace />} />
-      <Route element={<MainLayout />}>
-        <Route path="/orders" element={<OrdersPage />} />
-        <Route path="/orders/:orderId" element={<OrderPage />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<Navigate to="/orders" replace />} />
+        <Route element={<MainLayout />}>
+          <Route path="/orders" element={<OrdersPage />} />
+          <Route path="/orders/:orderId" element={<OrderPage />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
--- src/pages/OrderPage.jsx
+++ src/pages/OrderPage.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router";
 const OrderPage = () => {
   const { orderId } = useParams();
 
-  return <div>OrderPage {orderId}</div>;
+  return <h1>OrderPage {orderId}</h1>;
 };
 
 export default OrderPage;
