25d77441f1d655fdc2fa133a8540fa42b468157b
Serhii Popov <serhii.popov@codeit.pro>
2025-07-08 12:23:27 +0300

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { Suspense, lazy } from "react";
 import { Navigate, Route, Routes } from "react-router";
+
+import ErrorBoundary from "./components/ErrorBoundary";
 import { centerScreen } from "../styles/sharedClasses";
 
 const OrdersPage = lazy(() => import("./pages/OrdersPage"));
@@ -8,15 +10,17 @@ const MainLayout = lazy(() => import("./layouts/MainLayout"));
 
 function App() {
   return (
-    <Suspense fallback={<div className={centerScreen}>Loading...</div>}>
-      <Routes>
-        <Route path="/" element={<Navigate to="/orders" replace />} />
-        <Route element={<MainLayout />}>
-          <Route path="/orders" element={<OrdersPage />} />
-          <Route path="/orders/:orderId" element={<OrderPage />} />
-        </Route>
-      </Routes>
-    </Suspense>
+    <ErrorBoundary>
+      <Suspense fallback={<div className={centerScreen}>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Navigate to="/orders" replace />} />
+          <Route element={<MainLayout />}>
+            <Route path="/orders" element={<OrdersPage />} />
+            <Route path="/orders/:orderId" element={<OrderPage />} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </ErrorBoundary>
   );
 }
 
--- src/pages/OrdersPage.jsx
+++ src/pages/OrdersPage.jsx
@@ -26,34 +26,32 @@ const OrdersPage = () => {
   }
 
   return (
-    <div className="min-h-screen p-10">
-      <div className="max-w-3xl flex flex-col mx-auto">
-        {/* Header */}
-        <div className="flex items-center justify-between">
-          <div>
-            <h1 className="lg:text-4xl md:text-2xl text-xl dark:text-white tracking-wide font-semibold">
-              Orders
-            </h1>
-            <p className="text-gray-500 font-light">
-              There are {orders.length} orders.
-            </p>
-          </div>
-          <OrderStatusSelect />
+    <div className="p-10 flex flex-col">
+      {/* Header */}
+      <div className="flex items-center justify-between">
+        <div>
+          <h1 className="lg:text-4xl md:text-2xl text-xl dark:text-white tracking-wide font-semibold">
+            Orders
+          </h1>
+          <p className="text-gray-500 font-light">
+            There are {orders.length} orders.
+          </p>
         </div>
+        <OrderStatusSelect />
+      </div>
 
-        {/* Orders Cards */}
-        <div className="mt-10 space-y-4">
-          {orders.map((order) => (
-            <OrderCard
-              key={order.id}
-              id={order.id}
-              orderDate={order.orderDate}
-              status={order.status}
-              total={order.total}
-              clientName={order.clientName}
-            />
-          ))}
-        </div>
+      {/* Orders Cards */}
+      <div className="mt-10 space-y-4">
+        {orders.map((order) => (
+          <OrderCard
+            key={order.id}
+            id={order.id}
+            orderDate={order.orderDate}
+            status={order.status}
+            total={order.total}
+            clientName={order.clientName}
+          />
+        ))}
       </div>
     </div>
   );
