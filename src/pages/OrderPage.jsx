3756f9e1bbd871208a895af92a4fa5913f2fbb38
Serhii Popov <serhii.popov@codeit.pro>
2025-07-16 21:22:48 +0300

    table body added to ProductList

--- src/pages/OrderPage.jsx
+++ src/pages/OrderPage.jsx
@@ -87,30 +87,45 @@ const ProductList = ({ products }) => (
     </div>
 
     <div className="hidden sm:block mt-10 bg-neutral-50 rounded-lg rounded-b-none space-y-4 p-10">
-      {products.map((item) => (
-        <div key={item.id} className="flex justify-between">
-          <div className="space-y-4 basis-[60%]">
-            <p className="text-gray-400 font-thin">Product name</p>
-            <p className="text-base font-semibold">{item.name}</p>
-          </div>
-          <div className="space-y-4 basis-[10%]">
-            <p className="text-gray-400 font-thin">Qty.</p>
-            <p className="text-base font-semibold">{item.quantity}</p>
-          </div>
-          <div className="space-y-4 basis-[15%]">
-            <p className="text-gray-400 font-thin">Item price</p>
-            <p className="text-base font-semibold">
-              {formatCurrency(item.unitPrice)}
-            </p>
-          </div>
-          <div className="space-y-4 basis-[15%]">
-            <p className="text-gray-400 font-thin">Total</p>
-            <p className="text-base font-semibold">
-              {formatCurrency(item.lineTotal)}
-            </p>
-          </div>
-        </div>
-      ))}
+      <table className="w-full border-collapse">
+        <thead className="hidden">
+          <tr>
+            <th>Product name</th>
+            <th>Qty.</th>
+            <th>Item price</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((item) => (
+            <tr
+              key={item.id}
+              className="flex justify-between w-full mb-4 last:mb-0"
+            >
+              <td className="space-y-4 basis-[60%]">
+                <p className="text-gray-400 font-thin">Product name</p>
+                <p className="text-base font-semibold">{item.name}</p>
+              </td>
+              <td className="space-y-4 basis-[10%]">
+                <p className="text-gray-400 font-thin">Qty.</p>
+                <p className="text-base font-semibold">{item.quantity}</p>
+              </td>
+              <td className="space-y-4 basis-[15%]">
+                <p className="text-gray-400 font-thin">Item price</p>
+                <p className="text-base font-semibold">
+                  {formatCurrency(item.unitPrice)}
+                </p>
+              </td>
+              <td className="space-y-4 basis-[15%]">
+                <p className="text-gray-400 font-thin">Total</p>
+                <p className="text-base font-semibold">
+                  {formatCurrency(item.lineTotal)}
+                </p>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   </>
 );
